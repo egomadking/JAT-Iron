dc737ec5e1c2811f0d09d845f3a8e69e800f94b6
Roy Mosby <ego.madking@gmail.com>
2021-05-24 21:05:05 -0400

    validates title, posted, status

--- jat-iron-api/app/models/job.rb
+++ jat-iron-api/app/models/job.rb
@@ -1,4 +1,8 @@
 class Job < ApplicationRecord
   belongs_to :job_search
   has_many :notes
+
+  validates :title, presence: true
+  validates :posted, presence: true
+  validates :status, inclusion: {in: ["new", "applied", "interviewing", "offer", "accepted", "rejected", "decline", "closed"]}
 end

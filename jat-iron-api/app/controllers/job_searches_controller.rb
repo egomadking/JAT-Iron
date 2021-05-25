3db2232819907f655daf54b59dce639b060b184a
Roy Mosby <ego.madking@gmail.com>
2021-05-24 22:06:10 -0400

    defines show action

--- jat-iron-api/app/controllers/job_searches_controller.rb
+++ jat-iron-api/app/controllers/job_searches_controller.rb
@@ -1,2 +1,7 @@
 class JobSearchesController < ApplicationController
+
+  def show
+    job_search = JobSearch.find_by(id: params[:id])
+    render json: JobSearchSerializer.new(job_search).to_serialized_json
+  end
 end
--- jat-iron-api/app/controllers/jobs_controller.rb
+++ jat-iron-api/app/controllers/jobs_controller.rb
@@ -1,2 +1,6 @@
 class JobsController < ApplicationController
+  def show
+    job = Job.find_by(params[:id])
+    render json: JobSerializer.new(job).to_serialized_json
+  end
 end

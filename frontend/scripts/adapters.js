a6f6370c3a42f9eb0b211e7754b0b604a19d8167
Roy Mosby <ego.madking@gmail.com>
2021-05-25 14:49:58 -0400

    adds getJobSearch, corrects getJob typo

--- frontend/scripts/adapters.js
+++ frontend/scripts/adapters.js
@@ -10,8 +10,15 @@ class Adapter {
         console.dir(json)
       })
   }
+  getJobSearch(id){
+    fetch(`${this.baseUrl}/job_searches/${id}`)
+      .then(resp => resp.json())
+      .then(json =>{
+        console.dir(json)
+      })
+  }
   getJob(id){
-    fetch(`${this.baseUrl}/jobs`)
+    fetch(`${this.baseUrl}/jobs/${id}`)
       .then(resp => resp.json())
       .then(json => {
         console.dir(json)

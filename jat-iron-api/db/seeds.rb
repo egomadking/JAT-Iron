0ec3d8cb1ad3bb0cdcc41d47c41785b36d067bfa
Roy Mosby <ego.madking@gmail.com>
2021-05-24 21:05:36 -0400

    adds seeds

--- jat-iron-api/db/seeds.rb
+++ jat-iron-api/db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def f_recruiter
+
+end
+
+def f_email
+
+end
+
+def f_phone
+
+end
+
+def f_description
+
+end
+
+def f_note
+  Faker::Lorem.sentences(number: 2).join
+end
+
+search = JobSearch.create!(name: "test search")
+
+search.jobs << Job.new(title: "Full-stack web developer", posted: 20210501, closed: 20210530, description: Faker::Lorem.paragraphs(number: 5).join)
+search.jobs << Job.new(title: "Front-end web developer", posted: 20210504, closed: 20210523, description: Faker::Lorem.paragraphs(number: 5).join)
+search.jobs << Job.new(title: "Full-stack web developer", posted: 20210420, closed: 20210526, description: Faker::Lorem.paragraphs(number: 5).join)
+search.jobs << Job.new(title: "Full-stack web developer", posted: 20210512, closed: 20210610, description: Faker::Lorem.paragraphs(number: 5).join)
+
+job = Job.first
+job_2 = Job.second
+
+
+
+job.notes << Note.new(content: f_note)
+job.notes << Note.new(content: f_note)
+job_2.notes << Note.new(content: "Call me maybe.")

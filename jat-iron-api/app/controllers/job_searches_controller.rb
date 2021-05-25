6d417739665bfeff6f7ccbf43fb9ff285ef71922
Roy Mosby <ego.madking@gmail.com>
2021-05-25 08:33:09 -0400

    adds index action, placeholders for create, delete

--- jat-iron-api/app/controllers/job_searches_controller.rb
+++ jat-iron-api/app/controllers/job_searches_controller.rb
@@ -1,7 +1,20 @@
 class JobSearchesController < ApplicationController
 
+  def index
+    job_searches = JobSearch.all
+    render json: JobSearchSerializer.new(job_searches).to_serialized_json
+  end
+
   def show
     job_search = JobSearch.find_by(id: params[:id])
     render json: JobSearchSerializer.new(job_search).to_serialized_json
+  end
+
+  def create
+    #TODO: JobSearches create action
+  end
+
+  def delete
+    #TODO: JobSearches delete action
   end
 end

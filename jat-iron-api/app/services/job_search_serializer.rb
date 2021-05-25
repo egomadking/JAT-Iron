6e1585c4930b6141e40a5d882eeaccfbf98ae3f1
Roy Mosby <ego.madking@gmail.com>
2021-05-25 08:34:10 -0400

    adds id to serializer

--- jat-iron-api/app/services/job_search_serializer.rb
+++ jat-iron-api/app/services/job_search_serializer.rb
@@ -4,7 +4,7 @@ class JobSearchSerializer
   end
 
   def to_serialized_json
-    @job_search_obj.to_json(only: :name,
+    @job_search_obj.to_json(only: [:id, :name],
         include: {jobs:{ except: [:created_at, :updated_at]}}
       )
   end

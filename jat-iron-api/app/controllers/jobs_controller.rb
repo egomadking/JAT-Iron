bcc6aae242e22be459d5ac2ef10fe3af62060df3
Roy Mosby <ego.madking@gmail.com>
2021-05-25 08:33:56 -0400

    adds placeholders for create, update, delete

--- jat-iron-api/app/controllers/jobs_controller.rb
+++ jat-iron-api/app/controllers/jobs_controller.rb
@@ -3,4 +3,16 @@ class JobsController < ApplicationController
     job = Job.find_by(id: params[:id])
     render json: JobSerializer.new(job).to_serialized_json
   end
+
+  def create
+    #TODO: Jobs create action
+  end
+
+  def update
+    #TODO: Jobs update action
+  end
+
+  def delete
+    #TODO: Jobs delete action
+  end
 end
--- jat-iron-api/app/controllers/notes_controller.rb
+++ jat-iron-api/app/controllers/notes_controller.rb
@@ -1,2 +1,13 @@
 class NotesController < ApplicationController
+  def create
+    #TODO: Notes create action
+  end
+
+  def update
+    #TODO: Notes update action
+  end
+
+  def delete
+    #TODO: Notes delete action
+  end
 end

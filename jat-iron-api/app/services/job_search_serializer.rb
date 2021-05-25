2bead8ead56bc92e57c47783c09976c84b7742ec
Roy Mosby <ego.madking@gmail.com>
2021-05-24 22:25:05 -0400

    corrects initialize

--- jat-iron-api/app/services/job_search_serializer.rb
+++ jat-iron-api/app/services/job_search_serializer.rb
@@ -1,10 +1,10 @@
 class JobSearchSerializer
   def initialize(job_search_obj)
-    @job_search_obj
+    @job_search_obj = job_search_obj
   end
 
   def to_serialized_json
-    @job_search_obj.to_json(only: [:name],
+    @job_search_obj.to_json(only: :name,
         include: {jobs:{ except: [:created_at, :updated_at]}}
       )
   end

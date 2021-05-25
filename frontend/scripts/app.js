8b95cb844aad79ec121b6b4b93cd7b8ff2f29ba4
Roy Mosby <ego.madking@gmail.com>
2021-05-25 12:33:15 -0400

    add hamburger event listener

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", init)
 
+const adapter = new Adapter("http://127.0.0.1:3000")
+
 function init(evt){
-  console.log("hello world")
+  
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.from(document.querySelectorAll('.navbar-burger'));
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
 }

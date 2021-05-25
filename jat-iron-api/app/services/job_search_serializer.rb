8b0255ac534e8fcc6731471e087b91bb1c884524
Roy Mosby <ego.madking@gmail.com>
2021-05-24 22:07:07 -0400

    creates serializer

--- jat-iron-api/app/services/job_search_serializer.rb
+++ jat-iron-api/app/services/job_search_serializer.rb
@@ -1,3 +1,11 @@
 class JobSearchSerializer
-  
+  def initialize(job_search_obj)
+    @job_search_obj
+  end
+
+  def to_serialized_json
+    @job_search_obj.to_json(only: [:name],
+        include: {jobs:{ except: [:created_at, :updated_at]}}
+      )
+  end
 end
--- jat-iron-api/app/services/job_serializer.rb
+++ jat-iron-api/app/services/job_serializer.rb
@@ -1,3 +1,13 @@
 class JobSerializer
+  def initialize(job_obj)
+    @job_search_obj = job_obj
+  end
+
+  def to_serialized_json
+    @job_search_obj.to_json(
+      except: [:created_at, :updated_at],
+      include: :notes
+    )
+  end
 
 end

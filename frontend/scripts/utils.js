220ebbac24f65cfc24f0d687fb02a37efc2d3e3a
Roy Mosby <ego.madking@gmail.com>
2021-05-26 17:31:18 -0400

    formats file with prettier

--- frontend/scripts/utils.js
+++ frontend/scripts/utils.js
@@ -1,26 +1,28 @@
-class Utils{
-
-  createElement({el, classes}){
-    if(typeof el != "string"){
-      throw("'el' argument is not a string")
+class Utils {
+  createElement({ el, classes }) {
+    if (typeof el != 'string') {
+      throw "'el' argument is not a string";
     }
-    if(typeof classes != "string" && !(classes instanceof Array) && typeof classes != "undefined"){
-      throw (`'classes' param << ${classes} >> is not a string, array, or empty`)
+    if (
+      typeof classes != 'string' &&
+      !(classes instanceof Array) &&
+      typeof classes != 'undefined'
+    ) {
+      throw `'classes' param << ${classes} >> is not a string, array, or empty`;
     }
-    let element = document.createElement(el)
-    switch(typeof classes){
-      case "string":
+    let element = document.createElement(el);
+    switch (typeof classes) {
+      case 'string':
         element.classList.add(classes);
         break;
-      case "object":
-        classes.forEach( klass => {
-          element.classList.add(klass)
-        })
+      case 'object':
+        classes.forEach((klass) => {
+          element.classList.add(klass);
+        });
         break;
-      case "undefined":
+      case 'undefined':
         break;
       default:
-
     }
     return element;
   }

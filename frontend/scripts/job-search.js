5661a4d3df3ec65d99486da373030ab3d08753e2
Roy Mosby <ego.madking@gmail.com>
2021-05-26 18:52:23 -0400

    adds getter and setter for id

--- frontend/scripts/job-search.js
+++ frontend/scripts/job-search.js
@@ -1,5 +1,16 @@
 class JobSearch {
   constructor(id) {
-    this.id = id;
+    this._id = id || 0;
   }
+  get id() {
+    return this._id;
+  }
+
+  set id(id) {
+    this._id = id;
+  }
+
+  //TODO: loadJobs
+  //TODO: insertJobs into pane
+  //TODO: sortJobsByStatus
 }

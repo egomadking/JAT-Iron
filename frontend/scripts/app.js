49383c146bc964385b2dcaa03c173d6f729e7c44
Roy Mosby <ego.madking@gmail.com>
2021-05-27 21:30:44 -0400

    loads job seach chooser if none in sessionStorage

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -3,14 +3,24 @@ document.addEventListener('DOMContentLoaded', init);
 const adapter = new Adapter('http://127.0.0.1:3000');
 const _ = new Utils();
 const ui = new Ui();
+const jobSearch = new JobSearch();
+
+let debugJobSearch;
 
 function init(evt) {
   let storedId = adapter.getStoredId();
   if (storedId) {
-    adapter.getJobSearch(storedId);
+    adapter.getJobSearch(storedId, function (json) {
+      jobSearch.id = storedId;
+      jobSearch.jobs = json.jobs;
+    });
+    //TODO: add jobs into JobSearch and populate page
   } else {
     console.log('no id stored yet');
-    adapter.getJobSearches();
+    adapter.getJobSearches(function (json) {
+      debugJobSearch = json;
+      jobSearch.buildJobSearchForm(json);
+    });
   }
   /*
 

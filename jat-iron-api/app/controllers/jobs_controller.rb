ef8effeb93bcf0818cceb398ea9f4700552f6672
Roy Mosby <ego.madking@gmail.com>
2021-05-29 11:41:27 -0400

    fleshes out strong params and update action

--- jat-iron-api/app/controllers/jobs_controller.rb
+++ jat-iron-api/app/controllers/jobs_controller.rb
@@ -5,14 +5,29 @@ class JobsController < ApplicationController
   end
 
   def create
+    byebug
+    job = Job.find_by(id: params[:job_id])
+
     #TODO: Jobs create action
   end
 
   def update
-    #TODO: Jobs update action
+    job = Job.find_by(id: params[:id])
+    if job
+      job.update(job_params)
+      render json: JobSerializer.new(job).to_serialized_json, status:200
+    else
+    
+    end
   end
 
   def delete
+    byebug
     #TODO: Jobs delete action
   end
+end
+
+
+def job_params
+  params.require(:job).permit(:title, :status, :url, :company_logo, :recruiter_name, :recruiter_email, :recruiter_phone, :poc_notes, :posted, :closed, :description)
 end

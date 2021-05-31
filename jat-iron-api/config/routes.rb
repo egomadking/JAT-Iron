b75c402bd99827b30e33a0a6495cfd126c5dbeb1
Roy Mosby <ego.madking@gmail.com>
2021-05-30 21:28:45 -0400

    limits routes to those used

--- jat-iron-api/config/routes.rb
+++ jat-iron-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :jobs
-  resources :job_searches
+  resources :jobs, only: [:show, :create, :update, :destroy]
+  resources :job_searches, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
